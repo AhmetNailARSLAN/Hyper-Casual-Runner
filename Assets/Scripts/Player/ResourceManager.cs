53f2d5b8513fdd96e9a3a745923290b8aa0f0c9d
AhmetNailARSLAN <41548298+AhmetNailARSLAN@users.noreply.github.com>
2024-11-15 14:51:13 +0300

    Resource Update

---
+++ Assets/Scripts/Player/ResourceManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI resourceText; // Kaynak texti
+
+    private int currentResource = 0; // Toplam kaynak
+
+    private void Start()
+    {
+        resourceText.text = currentResource.ToString();
+    }
+
+    // Kaynak toplama
+    public void AddResource(int resourceCount)
+    {
+        currentResource += resourceCount;
+        resourceText.text = currentResource.ToString();
+    }
+
+    // Kaynak kaybetme
+    public void ReduceResource(int resourceCount)
+    {
+        currentResource -= resourceCount;
+        if (currentResource < 0) currentResource = 0;
+        resourceText.text = currentResource.ToString();
+    }
+    // Kaynak çarpma
+    public void MultiplyResource(int resourceCount)
+    {
+        currentResource *= resourceCount;
+        resourceText.text = currentResource.ToString();
+    }
+    // Kaynak bölme
+    public void DivideResource(int resourceCount)
+    {
+        currentResource /= resourceCount;
+        if (currentResource < 0) currentResource = 0;
+        resourceText.text = currentResource.ToString();
+    }
+
+}
