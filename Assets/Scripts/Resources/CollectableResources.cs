e39170eaba7795901ecebd22553ee810b66ebcd1
AhmetNailARSLAN <41548298+AhmetNailARSLAN@users.noreply.github.com>
2024-11-17 13:51:21 +0300

    CollectableResources

---
+++ Assets/Scripts/Resources/CollectableResources.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CollectableResources : MonoBehaviour
+{
+    [SerializeField] private int resourceAmount = 1;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<ResourceManager>().AddResource(resourceAmount);
+            Destroy(gameObject);
+        }
+    }
+}
